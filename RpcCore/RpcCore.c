c92891ea03f9a215218e4cfc30ad3f016e8025a5
silverf0x <silverf0x@users.noreply.github.com>
2019-01-15 21:13:03 +0100

    Fix #25: out of bounds read in RpcCoreInit

--- RpcCore/RpcCore.c
+++ RpcCore/RpcCore.c
@@ -289,7 +289,7 @@ VOID* __fastcall RpcCoreInit(BOOL bForce)
 	if (GetSystemDirectoryW(RpcRuntimePath,_countof(RpcRuntimePath))==0) goto End;
 	StringCbPrintfW(RpcRuntimePath,sizeof(RpcRuntimePath),L"%s\\rpcrt4.dll",RpcRuntimePath);
 	RuntimVersion=GetModuleVersion(RpcRuntimePath);
-	for (i = 0; i < sizeof(RPC_CORE_RUNTIME_VERSION); i++)
+	for (i = 0; i < _countof(RPC_CORE_RUNTIME_VERSION); i++)
 	{
 		if (bForce && ((RuntimVersion & 0xFFFFFFFF00000000) == (RPC_CORE_RUNTIME_VERSION[i] & 0xFFFFFFFF00000000)))
 		{
