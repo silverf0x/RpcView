2ce4f6f7a248158d4f79738d72e54e6ff46adb2a
silverf0x <silverf0x@users.noreply.github.com>
2019-08-28 21:16:58 +0200

    Fix #35: Hang with VBS enabled due to EnumProcessModulesEx ignored return value

--- RpcCore/RpcCore.c
+++ RpcCore/RpcCore.c
@@ -223,11 +223,10 @@ BOOL WINAPI GetRpcServerAddressInProcess(DWORD Pid,RpcCoreInternalCtxt_T* pRpcCo
 	hProcess = ProcexpOpenProcess(PROCESS_ALL_ACCESS, FALSE, Pid);
 	if (hProcess == NULL) goto End;
 
-	EnumProcessModulesEx(hProcess, NULL, 0, &cbSize, LIST_MODULES_ALL);
-	if (cbSize == 0) goto End;
+    if (!EnumProcessModulesEx(hProcess, NULL, 0, &cbSize, LIST_MODULES_ALL)) goto End;
 	pHmodule = (HMODULE*)malloc(cbSize);
     if (pHmodule == NULL) goto End;
-	EnumProcessModulesEx(hProcess, pHmodule, cbSize, &cbSize, LIST_MODULES_ALL);
+    if (!EnumProcessModulesEx(hProcess, pHmodule, cbSize, &cbSize, LIST_MODULES_ALL)) goto End;
 
 	for(ULONG i=0;i<cbSize/sizeof(*pHmodule);i++)
 	{
